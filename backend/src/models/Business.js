60e99df6c90a64143bc5e838c7228d795d6799b6
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-14 19:48:47 +1100

    Feat:Onboaring-form/ Industry field in Business Model updated

--- backend/src/models/Business.js
+++ backend/src/models/Business.js
@@ -22,7 +22,8 @@ const BusinessSchema = new mongoose.Schema({
   // Industry - Required, must be a string, and trimmed
   industry: { 
     type: String, 
-    required: true, 
+    required: true,
+    trim: true, // To remove space if it is at the beginning or end of the string  
     maxlength: 50 
   },
 
