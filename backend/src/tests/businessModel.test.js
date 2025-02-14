e39aff6060fe74c4d891486dee67fb87500cb9ff
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-14 19:32:06 +1100

    Feat:Onboaring-form/ Unit test to validate Business model field working

--- backend/src/tests/businessModel.test.js
+++ backend/src/tests/businessModel.test.js
@@ -6,5 +6,27 @@ import User from '../models/User.js';
 // ------------------------------------------------------------------------------------------------------------------//
 // SECTION: Business Model Validations
 
+describe(' Business Model Validations', () => {
 
+    it('should require userId, businessName, industry, phone, and address', async () => {
+    const business = new Business({});
+
+    let error;
+    try {
+        await business.validate();
+    } catch (err) {
+        error = err;
+    }
+
+    expect(error).toBeDefined();
+    expect(error.errors.userId).toBeDefined();
+    expect(error.errors.businessName).toBeDefined();
+    expect(error.errors.industry).toBeDefined();
+    expect(error.errors.phone).toBeDefined();
+    expect(error.errors.address).toBeDefined();
+    });
+  
+    
+  
+});
 // -----------------------------------------------------------------------------------------------------------------
