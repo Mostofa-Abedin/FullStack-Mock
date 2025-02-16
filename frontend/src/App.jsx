d499f9cba39329c1a0e1603020b1fdf196d64bd9
perripez <pezzadkins@gmail.com>
2025-02-16 15:25:49 +1000

    Added hero text to home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import UserList from './components/UserList';
+import UserList from "./components/UserList";
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css"
 
-import HeroSection from './components/heroSection/heroSection';
+import HeroSection from "./components/heroSection/heroSection";
 
 function App() {
   return (
@@ -12,7 +13,14 @@ function App() {
       <div>
         <Navbar />
         <HeroSection />
+        <p className="hero-text">
+          We craft solutions that 
+          <span className="highlight red"> Attract</span>,
+          <span className="highlight blue"> Engage</span>, &{" "}
+          <span className="highlight yellow">Convert</span>.
+        </p>
         <h1>FullStack Mock App</h1>
+
         <UserList />
         <Footer />
       </div>
