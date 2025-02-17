40e86e369f2266c7177c38904efe4d1e4c190f2a
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-17 12:07:25 +1100

    feat:RBAC, Middleware to verify JWT token created

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,3 +1,13 @@
 //Import required libraries
 const express = require('express');
 const router = express.Router();
+const { authorizeAdmin } = require('../middlewares/authMiddleware');
+
+
+// Admin protected route via middleware
+// @Perri - Use this route for the admin dashboard.
+router.get('admin/dashboard', authorizeAdmin, (req, res) => {
+    res.json({ message: "Welcome to the Admin Dashboard." });
+});
+
+module.exports = router;
