d6a4333b7a16a805a9fa30b7990df4619069bfe4
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-17 12:10:27 +1100

    feat:RBAC, Client route updated

--- backend/src/routes/clientRoutes.js
+++ backend/src/routes/clientRoutes.js
@@ -4,3 +4,10 @@ const express = require('express');
 const router = express.Router();
 
 // We'll use this file for client dashboard.
+// @Perri - Use this route for the client dashboard
+
+router.get('/client/dashboard', authenticateUser, (req, res) => {
+    res.json({ message: "Welcome to the Client Dashboard." });
+});
+
+module.exports = router;
