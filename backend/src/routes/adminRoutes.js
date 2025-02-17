3a802d828b4321c94ffa3cf599242f725f50c95a
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-17 12:09:12 +1100

    feat:RBAC, Admin route updated

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,12 +1,12 @@
 //Import required libraries
 const express = require('express');
 const router = express.Router();
-const { authorizeAdmin } = require('../middlewares/authMiddleware');
+const { authenticateUser,authorizeAdmin } = require('../middlewares/authMiddleware');
 
 
 // Admin protected route via middleware
 // @Perri - Use this route for the admin dashboard.
-router.get('admin/dashboard', authorizeAdmin, (req, res) => {
+router.get('admin/dashboard', authenticateUser, authorizeAdmin, (req, res) => {
     res.json({ message: "Welcome to the Admin Dashboard." });
 });
 
