0a6e30780d606fd7302e7c1aa615b2348ac48151
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-23 14:48:55 +1100

    User Routes cleaned up,commnted and new route added, authUserOrAdmin middleware implemented

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,29 +1,37 @@
 // Import required libraries
 const express = require('express');
 const router = express.Router();
+
+// Import Controllers
 const { getAllUsers, createUser, registerUser, changePassword, updateUserProfile   } = require('../controllers/userController'); // Import controllers
 const { registerBusiness } = require('../controllers/businessFormController');
-const { authenticateUser } = require('../middlewares/authMiddleware');
 
-//Middlewares
+
+//Import Middlewares
+const { authenticateUser } = require('../middlewares/authMiddleware');
 const { authUserOrAdmin } = require('../middlewares/authUserOrAdmin');
 
 // Route: GET all users 
+// (Need to add authentication in the future) (Admin or Authenticated User)
 router.get('/', getAllUsers); 
 
-// Route: // Create a new user
+// Route: Create a new user
+// (Need to add authentication in the future) (Admin only)
 router.post('/', createUser);  
 
-// Route: Register a new user
+// Route: Register a new user (Public)
 router.post('/register', registerUser);
 
+// Route: Onboard business (Client only)
 //@Trenton- Can we move this to the newly created businessRoutes.js as this route falls under business-related logic. 
 router.post('/:id/onboarding', authenticateUser, registerBusiness)
 
+// Route: Change user password (Authenticated User only) 
+//@Trenton- You wrote this? It uses the authenticateUser middleware that verify if a token exist. However, it seems that we don't have any authorization. 
+// Which could imply that anybody with the right ID can change the password?
 router.patch('/:id/password', authenticateUser, changePassword)
 
-
-// Route: PATCH personal profile (only own profile or admin access)
-router.patch('/:id/profile');
+// Route: Update personal profile (Partial update allowed) (only own profile or admin access)
+router.patch('/:id/profile', authenticateUser, authUserOrAdmin, updateUserProfile);
 
 module.exports = router;
