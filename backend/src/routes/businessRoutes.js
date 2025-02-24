ba430b31046445da7c1b654730a3dcd58d96329d
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-24 11:02:32 +1100

    Business get all businesses added

--- backend/src/routes/businessRoutes.js
+++ backend/src/routes/businessRoutes.js
@@ -3,9 +3,14 @@ const express = require('express');
 const router = express.Router();
 const { authenticateUser } = require('../middlewares/authMiddleware');
 const { authUserOrAdmin } = require('../middlewares/authUserOrAdmin');
+const { getAllBusinessesWithUserDetails } = require('../controllers/businessController');
 
 // @Trenton- here
 //router.post('/:id/onboarding', authenticateUser, registerBusiness)
+
+
+// Route: GET all businesses (Publicly accessible) (For now  & testing)
+router.get('/', getAllBusinessesWithUserDetails);
 
 
 // Route: PATCH business details (only own business or admin access)
