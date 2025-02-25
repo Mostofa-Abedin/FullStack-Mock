8cf3ad2e89263c749e460c2535eac3b86d0fbc1e
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-02-25 12:41:13 +1100

    Business Detailis Route updated to avoid clashing with middleware

--- backend/src/routes/businessRoutes.js
+++ backend/src/routes/businessRoutes.js
@@ -14,6 +14,7 @@ router.get('/', getAllBusinessesWithUserDetails);
 
 
 // Route: PATCH business details (only own business or admin access)
-router.patch('/:id', authenticateUser, authUserOrAdmin, updateBusinessDetails);
+router.patch('/update/:businessId', authenticateUser, authUserOrAdmin, updateBusinessDetails);
+
 
 module.exports = router;
