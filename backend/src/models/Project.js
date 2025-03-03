b6f44059f190fc60ac4c11e80e126156b51b70bd
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-03-03 12:58:50 +1100

    Project model created

--- backend/src/models/Project.js
+++ backend/src/models/Project.js
@@ -1,1 +1,38 @@
 const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema(
+  {
+    clientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Links project to a client (User)
+      required: true,
+    },
+    projectName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["Upcoming", "In Progress", "Completed", "On Hold"],
+      default: "Upcoming",
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+module.exports = Project;
