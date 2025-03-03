18658de184c80375d53fe2030e0b7c74f9b729a4
perripez <pezzadkins@gmail.com>
2025-03-03 19:53:32 +1000

    Add client dashboard

---
+++ frontend/src/pages/ClientDashboard/clientDashboardPage.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react"; 
+import ClientDashboard from "../../components/clientDashboard/clientDashboard";
+import Header from "../../components/adminDashboard/adminHeader"; 
+import Sidebar from "../../components/adminDashboard/adminSidebar"; 
+import "./clientdashboardpage.css"
+
+const ClientDashboardPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top when the page loads
+  }, []);
+  return (
+    <div className="admin-dashboard-container">
+      <Header />
+      <div className="dashboard-layout">
+        <Sidebar />
+        <div className="dashboard-content">
+          <ClientDashboard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClientDashboardPage;
